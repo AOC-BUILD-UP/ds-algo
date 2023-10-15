ab1143380dfacc523848f2d97584179761670fbd
Nicholas shankland <nicholasshankland@Nicholass-MBP-2.attlocal.net>
2023-10-15 10:16:34 -0700

    added answers section

--- ts/src/questions/crystal_ball.ts
+++ ts/src/questions/crystal_ball.ts
@@ -15,40 +15,9 @@
  */
 
 export function crystal_ball_fast(arr: Array<number>): number {
-  const sqrt = Math.floor(Math.sqrt(arr.length));
-
-  for (let i = 0; i < arr.length; i += sqrt) {
-    if (arr[i] !== 1) {
-      continue;
-    }
-
-    const start = i - sqrt;
-    for (let i = start; i < arr.length; i++) {
-      if (arr[i] === 1) {
-        return i;
-      }
-    }
-  }
-
-  // Check end edge case
-  if (arr[arr.length - 1] === 1) {
-    const start = arr.length - sqrt;
-    for (let i = start; i < arr.length; i++) {
-      if (arr[i] === 1) {
-        return i;
-      }
-    }
-    return 0;
-  }
-
   return 0;
 }
 
 export function crystal_ball(arr: Array<number>): number {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === 1) {
-      return i;
-    }
-  }
   return 0;
 }
