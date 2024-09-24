2bdc770e1545d0eaa4d36417b47f4584f80196ec
Nicholas shankland <nicholasshankland@Nicholass-MacBook-Pro-2.local>
2024-09-24 06:34:59 +0100

    remove question answer

--- ts/src/questions/sorts/bubble_sort.ts
+++ ts/src/questions/sorts/bubble_sort.ts
@@ -7,14 +7,5 @@
  */
 
 export function bubble_sort(nums: Array<number>): Array<number> {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length - 1 - i; j++) {
-      if (nums[j] > nums[j + 1]) {
-        // Swap
-        [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]];
-      }
-    }
-  }
-
   return nums;
 }
